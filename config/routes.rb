cf85cbb47693cbf5bb3c750cd9bc97249e0d39bc
Alejandro Reyes <alejandroereyes@gmail.com>
2015-06-04 18:46:45 -0500

    deleted comment

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   root to: 'chat_room#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
+  #
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
