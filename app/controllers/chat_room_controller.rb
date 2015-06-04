f786f19acccccd7b873f811f005025f716791de0
Alejandro Reyes <alejandroereyes@gmail.com>
2015-06-04 16:53:45 -0500

    disable security

--- app/controllers/chat_room_controller.rb
+++ app/controllers/chat_room_controller.rb
@@ -4,6 +4,10 @@ class ChatRoomController < ApplicationController
     render json: ChatRoom.all
   end
 
+  # def time
+  #   render json: ChatRoom.order(created_at: :desc).
+  # end
+
   def create
     render json: ChatRoom.create(name: params[:name], message: params[:name])
   end
