a8ad3cc84d466d6095485a78baf511a501760389
Alejandro Reyes <alejandroereyes@gmail.com>
2015-06-04 17:21:55 -0500

    fixed bug with POST, message updates with message

--- app/controllers/chat_room_controller.rb
+++ app/controllers/chat_room_controller.rb
@@ -5,10 +5,10 @@ class ChatRoomController < ApplicationController
   end
 
   # def time
-  #   render json: ChatRoom.order(created_at: :desc).
+  #   ChatRoom.where(:created_at => @selected_date.beginning_of_day..@selected_date.end_of_day)
   # end
 
   def create
-    render json: ChatRoom.create(name: params[:name], message: params[:name])
+    render json: ChatRoom.create(name: params[:name], message: params[:message])
   end
 end
