d91f2854c9383dbc7293e13a226f328a73a58ded
Alejandro Reyes <alejandroereyes@gmail.com>
2015-06-04 16:11:17 -0500

    added route for root

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
   get '/confusa', to: 'chatroom#index'
+
+  root to: 'chatroom#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
